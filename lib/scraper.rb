a65bbe374e52f2ab235dee3d2444631ed4c03094
Dylan Crawshaw <dcrawsh@gmail.com>
2019-10-03 20:42:42 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,9 +2,11 @@ require 'open-uri'
 require 'pry'
 
 class Scraper
-
+  attr_accessor :name , :location, :profile_url
   def self.scrape_index_page(index_url)
-    
+  @name = index_url.css(a div.card-text-container h4)
+  @location = index_url.css(a div.card-text-container p)
+  @profile_url = index_url.css(.studentcard a)
   end
 
   def self.scrape_profile_page(profile_url)
